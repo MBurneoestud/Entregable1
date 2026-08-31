a310b1d9da3824b0613dda7e0a04328d2d9c6b1e
MBurneoestud <mburneor@estud.usfq.edu.ec>
2026-08-31 12:23:46 -0500

    Completado el archivo terminal.js. Incluye la clase Terminal, que es constituida por varias funciones que facilitan el manejo de la terminal. El archivo funciona como modulo ES. Terminal incluye la funcion async showMenu().

--- src/utils/terminal.js
+++ src/utils/terminal.js
@@ -1,6 +1,6 @@
-const readline = require('readline');
+import readline from 'readline';
 
-class Terminal {
+export class Terminal {
     constructor() {
         this.rl = readline.createInterface({
             input: process.stdin,
@@ -20,5 +20,35 @@ class Terminal {
         console.log(message);
     }
 
+    printHeader(title) {
+        console.log('\n' + '='.repeat(50));
+        console.log(`  ${title}`);
+        console.log('='.repeat(50) + '\n');
+    }
+
+    async showMenu(options) {
+        this.print('Select an option:');
+        options.forEach((option, index) => {
+            this.print(` ${index +1}. ${option}`);
+        });
+
+        const answer = await this.ask('\nEnter your choice: ');
+        const choice = parseInt(answer);
+
+        if(isNaN(choice) || choice < 1 || choice > options.length) {
+            this.print('Invalid choice. Please try again.');
+            return this.showMenu(options);
+        }
+
+        return choice -1;
+    }
+
+    close() {
+        this.rl.close();
+    }
+
+    clear() {
+        console.clear();
+    }
 
 }
